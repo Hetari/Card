03598de739aaf7d948b1c544a61ee569069601ad
Ebraheem Hetari <108173819+Hetari@users.noreply.github.com>
2023-07-11 05:15:56 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function () {
+    const body = document.querySelector('body');
+    const card = document.querySelector('.card');
+    const footer = document.querySelector('footer');
+    // Add 'loaded' class to body and card to trigger the animation
+    body.classList.add('loaded');
+    card.classList.add('loaded');
+    footer.classList.add('loaded');
+});
+
+const icons = document.querySelectorAll('ul.icons li a i');
+icons.forEach(icon => {
+    icon.addEventListener('mouseover', function () {
+        this.classList.add('bx-tada');
+    });
+    /* mouseleave */
+    icon.addEventListener('mouseout', function () {
+        this.classList.remove('bx-tada');
+    });
+});
